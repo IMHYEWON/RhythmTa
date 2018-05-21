8e78e1166bb261c6223d5b73dc0ff0ca51547bc0
JINYEBAP <yeajin357@naver.com>
2018-05-21 15:48:49 +0900

    Reduce *Check, *Check1, *Check2 and Modify parameter
    char *Check, *Check1, *Check2 is unnecessary variables.
    Changed parameter to increase readability.
    
    Signed-off-by: JIN YE JIN <yeajin357@naver.com>

--- framework.c
+++ framework.c
@@ -125,17 +125,10 @@ void NoteCheck(void);
 
 
 // 2차원 배열을 아래로 떨어지게끔 해주는 함수
-char* Check; 
-char* Check1;
-char* Check2;
 void ShowNote(int n) {
 	for (int i = 0; i < 28; i++) {
-		ScreenPrint(2, i + 1, Note[27 - i + n]);
-
+		ScreenPrint(2, 28-i, Note[n+i]);
 	}
-	Check = Note[n];   // 2차원배열 0.5초마다 Note의 배열 중  28행에 떨어지는 값을 저장받는다. 
-	Check1 = Note[n - 1]; // 0.5초 지난 후 입력받더라도 great으로 인정해주는 부분
-	Check2 = Note[n + 1]; // rmrm
 }
 
 
