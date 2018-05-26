837e9a6f2e16534f3b02b54093dfcfa97610feff
JINYEBAP <yeajin357@naver.com>
2018-05-26 21:12:01 +0900

    Add comment
    Add comment in Note.h.
    
    Signed-off-by: JIN YE JIN <yeajin357@naver.com>

--- Note.h
+++ Note.h
@@ -1,7 +1,8 @@
 #pragma once
 #include<string>
 using namespace std;
-// 노트에 해당하는 변수 선언
+
+// 입력 키 종류를 상수로 선언
 #define NumOfKey 10
 #define none 0
 #define a 1
@@ -13,12 +14,8 @@ using namespace std;
 #define aj 7
 #define ks 8
 #define dl 9
-typedef struct KeyNote {
-	string inputKey;
-	string nKey;
-}KeyNote;
-KeyNote KeyIndex[NumOfKey]; //노트 개수만큼 구조체 선언
 
+// 노트 종류를 string으로 선언
 string nKeyNone = "                                      ";
 string nKeyL = "                                ■■■";
 string nKeyK = "                          ■■■";
@@ -29,3 +26,12 @@ string nKeyA = "■■■";
 string nKeyAJ = "■■■              ■■■";
 string nKeySK = "      ■■■              ■■■";
 string nKeyDL = "            ■■■              ■■■";
+
+// 입력 키(inputKey)와 노트 종류(nKey)를 구조체로 선언
+typedef struct KeyNote {
+	string inputKey;
+	string nKey;
+}KeyNote;
+
+//입력 키&노트 종류 개수만큼 구조체 배열 선언
+KeyNote KeyIndex[NumOfKey]; 
